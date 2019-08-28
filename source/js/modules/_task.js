7a376ac3d4639a3b434888db5c49a570a59336fb
Sane <tretyakvl@gmail.com>
2019-08-28 13:49:59 +0300

    Adds the ability to update task on dblclick

--- source/js/modules/_task.js
+++ source/js/modules/_task.js
@@ -1,8 +1,29 @@
 const completeTask = require('./helpers/completeTask')
+const storage = require('./helpers/storage')
 const $task = $($('template').prop('content')).find('.task')
 
 $task.click(event => {
   if (event.target.matches('[type="checkbox"]')) {
     completeTask($(event.delegateTarget))
+  }
+})
+
+$task.dblclick(event => {
+  if (event.target.matches('[type="text"]')) {
+    const $parent = $(event.delegateTarget)
+    const $input = $(event.target)
+    $input.attr('readonly', false)
+
+    const blurHandler = (event) => {
+      storage.update(task => {
+        if (task.id === $parent.data('id')) {
+          task.value = $input.val()
+        }
+
+        return task
+      })
+    }
+
+    $input.blur(blurHandler)
   }
 })
