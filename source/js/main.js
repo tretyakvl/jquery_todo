f82373630798516cd4c36358ed4e71a24488b439
Sane <tretyakvl@gmail.com>
2019-08-28 19:47:33 +0300

    Adds tasks counter

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,7 @@
 
 $(() => {
   require('./modules/_header-date')
+  require('./modules/_add')
   require('./modules/_task') // Should be before _tasks
   require('./modules/_tasks')
-  require('./modules/_add')
 })
--- source/js/modules/_tasks.js
+++ source/js/modules/_tasks.js
@@ -1,4 +1,19 @@
 const storage = require('./helpers/storage')
 const addTask = require('./helpers/addTask')
 
+const tasksList = document.querySelector('.tasks__list')
+const $counter = $('.footer__count')
+const observer = new MutationObserver(observerCallback)
+const observerOptions = {
+  childList: true
+}
+function observerCallback () {
+  const count = tasksList.children.length
+  const s = count === 1 ? '' : 's'
+
+  $counter.text(`${count} item${s} left`)
+}
+
+observer.observe(tasksList, observerOptions)
+
 storage.getTasks().forEach(task => addTask(task))
